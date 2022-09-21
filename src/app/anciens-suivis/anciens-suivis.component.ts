70a57edf26b256ddfa4a1c63d4be75073ac5b4ef
victoria-schwindenhammer <vicschwindenhammer@gmail.com>
2022-09-22 00:40:01 +0200

    ancien suivi tableau

--- src/app/anciens-suivis/anciens-suivis.component.ts
+++ src/app/anciens-suivis/anciens-suivis.component.ts
@@ -15,16 +15,30 @@ export class AnciensSuivisComponent implements OnInit {
     dates :any = []
     questionnaire: any = [];
     questions: any = [];
-    typequestion: any = []
+    typequestion: any = [];
+    careplanid: any = [];
+    diagnostiques: any = [];
+    ordonnances: any = [];
+    popupquest =false;
+    popupordo =false;
+    questaffiche: number=0;
+    ordoaffiche: number=0;
 
     constructor(private router: Router, private http: HttpClient) {
     }
 
     ngOnInit() {
         this.http.get(" https://fhir.alliance4u.io/api/questionnaire-response?source.reference=Patient/007").subscribe(data => {
-            this.questionnairesreponse = data
+            this.questionnairesreponse = data 
             for (let questionnairereponse of this.questionnairesreponse){
-                this.dates.push((new Date(questionnairereponse.authored)).getDate()+"/"+((new Date(questionnairereponse.authored)).getMonth()+1)+"/"+(new Date(questionnairereponse.authored)).getFullYear())
+                var mois=((new Date(questionnairereponse.authored)).getMonth()+1).toString()
+                console.log(mois)
+                if((new Date(questionnairereponse.authored)).getMonth()+1<10){
+                    mois ="0"+((new Date(questionnairereponse.authored)).getMonth()+1)
+                }
+                this.dates.push((new Date(questionnairereponse.authored)).getDate()+"/"+mois+"/"+(new Date(questionnairereponse.authored)).getFullYear())
+                this.careplanid.push((questionnairereponse.basedOn[0].reference))
+
             }
             
         })
@@ -35,6 +49,17 @@ export class AnciensSuivisComponent implements OnInit {
                 this.typequestion.push(q.type)
             }
         })
+
+        this.http.get(" https://fhir.alliance4u.io/api/diagnostic-report?subject.reference=Patient/007").subscribe(data => {
+            this.diagnostiques = data 
+        })
+
+        this.http.get("https://fhir.alliance4u.io/api/medication-request").subscribe(data => {
+            this.ordonnances = data
+            console.log(this.ordonnances[0].medicationCodeableConcept.coding[0].display)
+        })
+
+        
     }
 
     transfoBoolean(boolean : Boolean ){
@@ -45,6 +70,30 @@ export class AnciensSuivisComponent implements OnInit {
             return "non"
         }
           
+        }
+
+        visuQuest(number : number){
+            this.questaffiche=number
+            this.popupquest=true
+            
+        }
+        
+        visuOrdo(number : number){
+            this.ordoaffiche=number
+            this.popupordo=true
+        }
+            
+        
+
+        fermepopupquest(){
+            this.popupquest=false
+
+        }
+
+        fermepopupordo(){
+            this.popupordo=false
+            
+
         }
 
 }
